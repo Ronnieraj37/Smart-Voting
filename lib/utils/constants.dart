ac7719c23b1120943bed78677f9390facd0d0443
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2023-04-14 01:42:41 +0530

    Made all constants Private

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,8 @@
 String infuraUrl =
-    "https://goerli.infura.io/v3/cce64a7776fb4e529f9986166c6c1492";
+    *Enter Your InfuraUrl*
 String ownerKey =
-    "6179993d608b4a4a5acfa019e1624b3a0a12337884865c3c633d2d682b134faf";
+    *Enter Your ownerKey*
 String voterKey =
-    "489dfb7172df11e18d908e4791ff146d8f5aced98d51f143099dde9b174881dd";
-String contractAddress1 = "0x018b59D2b831955371320812693D54e51E563223";
-String contractAddress2 = "0x82A3c16204aa62E454054D6eB49Cc65C64687318";
+    *Enter Your voterKey*
+String contractAddress1 = *Enter Your contractAddress1*
+String contractAddress2 = *Enter Your contractAddress2*
